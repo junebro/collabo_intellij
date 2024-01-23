a6dd094dfac0c50e7a88fe02c8903f0bd47fc1ab
june <june@naver.com>
2024-01-23 11:54:11 +0900

    두 개의 파일이 수정되었습니다.

--- src/ch01/hello.java
+++ src/ch01/hello.java
@@ -4,6 +4,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello01");
         System.out.println("hello02");
-        System.out.println("hello03");
+        System.out.println("hello003");
     }
 }
--- src/ch01/world.java
+++ src/ch01/world.java
@@ -3,6 +3,6 @@ package ch01;
 public class world {
     public static void main(String[] args) {
         System.out.println("world01");
-        System.out.println("world02");
+        System.out.println("world002");
     }
 }
