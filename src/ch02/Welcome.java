85603b1b4b9c8647dc5f6880e785fd1ddd050bdc
june <june@naver.com>
2024-01-23 12:55:24 +0900

    pull 하지 않은 데이터를 push하려고 시도합니다.

--- src/ch02/Welcome.java
+++ src/ch02/Welcome.java
@@ -3,6 +3,6 @@ package ch02;
 public class Welcome {
     public static void main(String[] args) {
         System.out.println("Welcome01");
-        System.out.println("Welcome02_written_by_hee");
+        System.out.println("Welcome02_written_by_heee");
     }
 }
